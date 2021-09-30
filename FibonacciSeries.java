b6b4fc5ee387bfeca96de597c1f3d6e8d0307b27
Ganesh <tinku.ganesh818@gmail.com>
2021-09-30 23:10:21 +0530

    New Pgm

---
+++ FibonacciSeries.java
@@ -0,0 +1,26 @@
+package com.first.java;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+
+		int a1 = 0, a2 = 1;
+
+		int count = 10;
+
+		System.out.print(a1 + " " + a2);
+
+		for (int i = 2; i <= count; i++) {
+
+			int a3 = a1 + a2;
+
+			System.out.print(" " + a3);
+
+			a1 = a2;
+			a2 = a3;
+
+		}
+
+	}
+
+}
